aa74113ccfa916a65937191bbecaecf85d60ee56
SGTSuPo <sgtsupo@gmail.com>
2024-06-25 04:11:57 +0700

    commit 3

---
+++ Database/Chon_Coso.sql
@@ -0,0 +1,7 @@
+﻿--CHỌN CƠ SỞ (TỪ SITE CHỦ)
+CREATE VIEW Get_Subscribes
+AS
+	SELECT TENCS=PUBS.description, TENSERVER=subscriber_server
+	 FROM sysmergepublications  PUBS, sysmergesubscriptions SUBS
+	 WHERE PUBS.pubid = SUBS.pubid AND  publisher <> subscriber_server
+GO
